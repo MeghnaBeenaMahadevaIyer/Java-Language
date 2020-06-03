6f3ba041d91d5b0a2438a655299c0e4fddaf6d81
Meghna Beena Mahadeva Iyer <56489743+MeghnaBeenaMahadevaIyer@users.noreply.github.com>
2020-06-03 21:00:08 +0530

    Add files via upload

---
+++ SampleHello.java
@@ -0,0 +1,16 @@
+package samplehello;
+/**
+ *
+ * @author Megzieboo
+ */
+public class SampleHello 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+    
+}
